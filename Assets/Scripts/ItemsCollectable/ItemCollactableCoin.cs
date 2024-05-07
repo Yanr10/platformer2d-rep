ffc1b578d1291fee3457f1af50d390e2d4b3f213
Yanr10 <yanrodrigues.silva@hotmail.com>
2024-05-06 21:07:37 -0300

    consertando erros nas moedas e colocando audiotrigger

--- Assets/Scripts/ItemsCollectable/ItemCollactableCoin.cs
+++ Assets/Scripts/ItemsCollectable/ItemCollactableCoin.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class ItemCollactableCoin : ItemCollactableBase
 {
-    
+    public Collider2D collider2D;
+
     protected override void OnCollect()
     {
         base.OnCollect();
         ItemManager.Instance.AddCoins();
-        
+        collider2D.enabled = false;
     }
     
 }
--- Assets/Scripts/ItemsCollectable/ItemCollactableMiniBall.cs
+++ Assets/Scripts/ItemsCollectable/ItemCollactableMiniBall.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class ItemCollactableMiniBall : ItemCollactableBase
 {
+
+    public Collider2D collider2D;
     protected override void OnCollect()
     {
         base.OnCollect();
         ItemManager.Instance.AddMiniBall();
+        collider2D.enabled = false;
     }
 }
